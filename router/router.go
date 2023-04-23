68714360dae43e18d00de6dfa5d283f870940e72
YakobusIP <82079465+YakobusIP@users.noreply.github.com>
2023-04-23 22:18:09 +0900

    re-add router with all endpoints

---
+++ router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"jobhun-backend.com/controller"
+)
+
+func Router() {
+	fmt.Println("Init router")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/get-all-mahasiswa", controller.GetAllMahasiswa).Methods("GET")
+
+	router.HandleFunc("/get-mahasiswa-on-id/{id}", controller.GetMahasiswaOnID).Methods("GET")
+
+	router.HandleFunc("/insert-mahasiswa", controller.InsertMahasiswa).Methods("POST")
+
+	router.HandleFunc("/update-mahasiswa", controller.UpdateMahasiswa).Methods("PUT")
+
+	router.HandleFunc("/delete-mahasiswa/{id}", controller.DeleteMahasiswa).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
